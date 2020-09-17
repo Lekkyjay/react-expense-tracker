b462107fa779cb8a0242b75015404beb18860fb0
Lekkyjay <lekkyjayjay@yahoo.com>
2020-09-17 09:10:06 +0200

    Updates

---
+++ precache-manifest.5605eab5dc1b47b03f3fc6755f6bc9c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc745d3a496d2b4881870ee492c1480",
+    "url": "/github.io/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "2af2635ad6f6dae9ff0b",
+    "url": "/github.io/react-expense-tracker/static/css/main.f3d06bd3.chunk.css"
+  },
+  {
+    "revision": "0f7dc4d5f00c7b99fddb",
+    "url": "/github.io/react-expense-tracker/static/js/2.bf055756.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/react-expense-tracker/static/js/2.bf055756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2af2635ad6f6dae9ff0b",
+    "url": "/github.io/react-expense-tracker/static/js/main.3cf6f5b9.chunk.js"
+  },
+  {
+    "revision": "08698aa5068855677940",
+    "url": "/github.io/react-expense-tracker/static/js/runtime-main.fcb69f5c.js"
+  }
+]);
